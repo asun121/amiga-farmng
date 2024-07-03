9c9fb32b4e52b06f3ef3bde732943ad4bc399ac8
asun121 <albert.yang.sun@gmail.com>
2024-07-03 12:55:37 -0400

    Fixed CANBus handling

--- src/amiga_navigate/amiga_navigate/canbus_handler.py
+++ src/amiga_navigate/amiga_navigate/canbus_handler.py
@@ -15,12 +15,17 @@ class CANBusHandler(Node):
             self.listener_callback,
             10)
         self.subscription  # prevent unused variable warning
-        self.ser = serial.Serial('/dev/ttyACM0', 115200)
+        try:
+            self.ser = serial.Serial('/dev/ttyACM0', 115200)
+        except:
+            self.ser = serial.Serial('/dev/ttyACM1', 115200)
 
     def listener_callback(self, msg):
         self.get_logger().info('Twist: "%s"' % msg)
-        twist_str = str(msg.linear.x) + " " + str(msg.angular.z)
-        self.ser.write(twist_str.encode('utf-8'))
+        #twist_str = str(msg.linear.x) + "," + str(msg.angular.z)
+        #self.ser.write(twist_str.encode('ascii'))
+
+        self.ser.write((str(msg.linear.x) + "," + str(msg.angular.z)).encode('utf-8'))
 
 
 def main(args=None):
